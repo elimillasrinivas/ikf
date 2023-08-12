b8e2b05f1be5c57a6cc70d2862d93a1350141e91
Srinivas Elimilla <114722049+elimillasrinivas@users.noreply.github.com>
2023-08-12 19:42:36 +0530

    Update Menu.jsx

--- client/src/components/Menu.jsx
+++ client/src/components/Menu.jsx
@@ -9,6 +9,56 @@ const Menu = () => {
         <Link to="/create-lead" className="btn btn-primary">Add Lead</Link>
         <Link to="/display-lead" className="btn btn-secondary">View Lead</Link>
       </div>
+
+       <div class="accordion accordion-flush mt-3 border" id="accordionFlushExample">
+        <div class="accordion-item">
+          <h2 class="accordion-header">
+            <button
+              class="accordion-button collapsed"
+              type="button"
+              data-bs-toggle="collapse"
+              data-bs-target="#flush-collapseOne"
+              aria-expanded="false"
+              aria-controls="flush-collapseOne"
+            >
+              View Lead
+            </button>
+          </h2>
+          <div
+            id="flush-collapseOne"
+            class="accordion-collapse collapse"
+            data-bs-parent="#accordionFlushExample"
+          >
+            <div class="accordion-body">
+             Here, you can check your details
+            </div>
+          </div>
+        </div>
+        <div class="accordion-item">
+          <h2 class="accordion-header">
+            <button
+              class="accordion-button collapsed"
+              type="button"
+              data-bs-toggle="collapse"
+              data-bs-target="#flush-collapseTwo"
+              aria-expanded="false"
+              aria-controls="flush-collapseTwo"
+            >
+             Add Lead
+            </button>
+          </h2>
+          <div
+            id="flush-collapseTwo"
+            class="accordion-collapse collapse"
+            data-bs-parent="#accordionFlushExample"
+          >
+            <div class="accordion-body">
+              Here, you can add your details
+            </div>
+          </div>
+        </div>
+      </div>
+      
     </div>
   );
 };
